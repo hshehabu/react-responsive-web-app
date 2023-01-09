91f8a8122c68447469eacc4601ee36276116b4a1
hshehabu <hamzashehabu457@gmail.com>
2023-01-09 08:37:28 +0300

    Featured products with auto animate

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -3,20 +3,27 @@ import css from "./Products.module.css";
 import Plane from "../../assets/plane.png";
 import { ProductsData } from "../../data/products";
 import { useState } from "react";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 const Products = () => {
+  const [parent] = useAutoAnimate();
+
   const [MenuProducts, setMenuProducts] = useState(ProductsData);
+
+  const filter = (type) => {
+    setMenuProducts(ProductsData.filter((product) => product.type === type));
+  };
   return (
     <div className={css.container}>
       <img src={Plane} alt="" />
       <h1>Our featured products</h1>
       <div className={css.products}>
         <ul className={css.menu}>
-          <li>All</li>
-          <li>Skin Care</li>
-          <li>Conditioners</li>
-          <li>Foundations</li>
+          <li onClick={() => setMenuProducts(ProductsData)}>All</li>
+          <li onClick={() => filter("skin care")}>Skin Care</li>
+          <li onClick={() => filter("conditioner")}>Conditioners</li>
+          <li onClick={() => filter("foundation")}>Foundations</li>
         </ul>
-        <div className={css.list}>
+        <div className={css.list} ref={parent}>
           {MenuProducts.map((products, i) => (
             <div className={css.product}>
               <div className="left-s">
